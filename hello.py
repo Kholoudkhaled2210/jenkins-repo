8285425407baf7210844b7036983b6ae5ab0c9ef
kholoud khaled <93731920+Kholoudkhaled2210@users.noreply.github.com>
2025-12-29 15:21:16 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def hello():
+    # دي الجملة اللي بتظهر في المتصفح
+    return "Hello from Jenkins and Docker!" 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
